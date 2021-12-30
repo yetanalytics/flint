44398b06428a94b6e3990fe8499fd72f7253725e
kelvinqian00 <kelvinqian2@gmail.com>
2021-12-30 12:55:36 -0500

    Add sugared triple vectors

--- src/main/syrup/sparql/spec/triple.clj
+++ src/main/syrup/sparql/spec/triple.clj
@@ -2,6 +2,19 @@
   (:require [clojure.spec.alpha :as s]
             [syrup.sparql.spec.axiom :as ax]))
 
+(defn- conj-set
+  [s x]
+  (if s (conj s x) #{x}))
+
+(defn- triples->nform
+  [triple-vecs]
+  (reduce (fn [acc [s p o]]
+            (if-not (and p o)
+              (assoc acc s nil)
+              (update-in acc [s p] conj-set o)))
+          {}
+          triple-vecs))
+
 (def subj-spec
   (s/or :subject ax/var-or-iri-spec))
 
@@ -28,10 +41,33 @@
                            :conform-keys true
                            :into [])))
 
+;; TODO: Optimize
+(def triples-vec-spec
+  (s/and (s/coll-of (s/tuple (s/nonconforming subj-spec)
+                             (s/nonconforming pred-spec)
+                             (s/nonconforming obj-spec))
+                    :min-count 1
+                    :kind vector?)
+         (s/conformer triples->nform)
+         (s/conformer normal-form-spec)))
+
+(def triples-spec
+  (s/and (s/or :sugared triples-vec-spec
+               :normal-form normal-form-spec)
+         ;; Remove s/or tag
+         (s/conformer second)))
+
 ;; NOTE: Subjects can be non-IRIs in SPARQL, but not in RDF
 ;; NOTE: RDF collections not supported (yet?)
 
 (comment
   (s/conform subj-spec '?subj)
-  (s/conform normal-form-spec
-             {'?subj {'?pred #{'?obj}}}))
+  (=
+   (s/conform triples-spec
+              {'?subj {'?p1 #{'?oa '?ob}
+                       '?p2 #{'?oa '?ob}}})
+   (s/conform triples-spec
+              [['?subj '?p1 '?oa]
+               ['?subj '?p1 '?ob]
+               ['?subj '?p2 '?oa]
+               ['?subj '?p2 '?ob]])))
