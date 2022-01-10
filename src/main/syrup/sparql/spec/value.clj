f25c52e6fd4b6570bd31870e407575921886e6d5
kelvinqian00 <kelvinqian2@gmail.com>
2022-01-10 17:39:32 -0500

    Add value conformer

--- src/main/syrup/sparql/spec/value.clj
+++ src/main/syrup/sparql/spec/value.clj
@@ -2,21 +2,38 @@
   (:require [clojure.spec.alpha :as s]
             [syrup.sparql.spec.axiom :as ax]))
 
-;; TODO: Add conformer
 (def values-clause-spec
-  (s/or :sparql
-        (s/and (s/map-of (s/coll-of ax/variable?)
-                         (s/coll-of (s/coll-of any?)))
-               (fn [m]
-                 (let [vars   (first (keys m))
-                       values (first (vals m))
-                       nv     (count vars)]
-                   (every? #(= nv (count %)) values))))
-        :clojure
-        (s/and (s/map-of ax/variable? (s/coll-of any?))
-               (fn [m]
-                 (let [values (vals m)
-                       nv     (count (first values))]
-                   (every? #(= nv (count %)) values))))))
+  (s/and
+   (s/or :sparql
+         (s/and (s/map-of (s/coll-of ax/variable?)
+                          (s/coll-of (s/coll-of any?)))
+                (fn [m]
+                  (let [vars   (first (keys m))
+                        values (first (vals m))
+                        nv     (count vars)]
+                    (every? #(= nv (count %)) values))))
+         :clojure
+         (s/and (s/map-of ax/variable? (s/coll-of any?))
+                (fn [m]
+                  (let [values (vals m)
+                        nv     (count (first values))]
+                    (every? #(= nv (count %)) values)))
+                (s/conformer
+                 (fn [m]
+                   (let [mfn    (fn [& ks] (vec ks))
+                         vars   (->> m keys vec)
+                         values (->> m vals (apply map mfn) vec)]
+                     {vars values})))))
+   (s/conformer second)))
 
 (s/def ::values values-clause-spec)
+
+(comment
+  (s/conform ::values '{[?foo ?bar] [[1 :a] [2 :b] [3 :c]]})
+
+  (s/conform ::values '{?foo [1 2 3]
+                        ?bar [:a :b :c]})
+  
+  (vals '{?foo [1 2 3]
+          ?bar [:a :b :c]})
+  (map (fn [& ks] ks) [1 2 3] [:a :b :c]))
---
+++ src/test/syrup/sparql/spec/value_test.clj
@@ -0,0 +1,11 @@
+(ns syrup.sparql.spec.value-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [clojure.spec.alpha :as s]
+            [syrup.sparql.spec.value :as vs]))
+
+(deftest conform-test
+  (testing "Conform VALUES clause"
+    (is (= '{[?foo ?bar] [[1 :a] [2 :b] [3 :c]]}
+           (s/conform ::vs/values '{[?foo ?bar] [[1 :a] [2 :b] [3 :c]]})
+           (s/conform ::vs/values '{?foo [1 2 3]
+                                    ?bar [:a :b :c]})))))
