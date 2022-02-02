982333e3e5fa3ad0eea9bd6908460200985c32ab
kelvinqian00 <kelvinqian2@gmail.com>
2022-02-02 10:39:16 -0500

    Add dev namespace

---
+++ src/dev/sandbox.clj
@@ -0,0 +1,31 @@
+(ns sandbox
+  (:import [org.apache.jena.update UpdateFactory]))
+
+(comment
+  (UpdateFactory/create
+   "PREFIX foo: <http://foo.org/>
+    DELETE WHERE { ?x foo:bar ?y } ;
+    
+    PREFIX foo: <http://foo-bar.org/>
+    PREFIX fii: <http://fii.org/>
+    DELETE WHERE { ?x foo:bar ?y . ?z fii:fum ?w }")
+  
+  (UpdateFactory/create
+   "BASE <http://foo.org/>
+    PREFIX bar: <bar>
+    DELETE WHERE { ?x bar:qux ?y . ?z <quu> ?w . } ;
+    
+    BASE <http://fii.org/>
+    PREFIX bii: <bii>
+    BASE <http://foe.org/>
+    PREFIX boe: <boe>
+
+    DELETE WHERE { ?x boe:decken ?y . ?z <joe> ?w . }")
+  
+  (UpdateFactory/create
+   "BASE <http://foo.org/>
+    DELETE WHERE { ?x <bar> ?y } ;
+    
+    BASE <http://baz.org/>
+    DELETE WHERE { ?x <qux> ?y }")
+  )
